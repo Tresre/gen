3590640a246b4d5fc61cf06c3b72f35816ef1f3d
Tresre <58750285+Tresre@users.noreply.github.com>
2022-05-05 13:06:34 -0400

    Create options.js

---
+++ assets/options.js
@@ -0,0 +1,22 @@
+phrase1 = [
+  "your beloved auntie",
+  "your country",
+  "your cousins car",
+  "your face",
+  "your father",
+  "your favourite Bond actor",
+  "your hat",
+  "your house",
+  "your hovercraft",
+  "your husband",
+  "your Kettle",
+  "your liver",
+  "your math teacher",
+  "your mother",
+  "your seat",
+  "your sense of style",
+  "your sister",
+  "your son",
+  "your wife"
+];
+
